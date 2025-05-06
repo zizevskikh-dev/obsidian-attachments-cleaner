cf5a3aef9e46c27305a260842c177f73a4231766
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-05-06 16:44:26 +0300

    changed cli atribute names

--- cli.py
+++ cli.py
@@ -6,7 +6,7 @@ class AttachmentsCleanerCLI:
     def __init__(self):
         self.parser = argparse.ArgumentParser(
             prog="Obsidian Attachments Cleaner, the CLI utility",
-            usage="[-run] [-silent]",
+            usage="[-r | -run] [-s | -show]",
             description="This simple CLI utility can help you to remove unused attachments in your obsidian vault",
             epilog="""Enjoy the Obsidian Attachments Cleaner ❤ ❤ ❤\nYou can see more information and examples in 'README.md' or GitHub:\nhttps://github.com/zizevskikh-dev/obsidian-attachments-cleaner.git\n\nObsidian Attachments Cleaner ver.2.0.1\nCreated by Aleksander Zizevskikh, 2025\nEmail: zizevskikh.dev@gmail.com""",
             formatter_class=argparse.RawTextHelpFormatter,
@@ -16,16 +16,18 @@ class AttachmentsCleanerCLI:
 
     def add_arguments_to_parser(self):
         self.parser.add_argument(
+            "-r",
             "-run",
             "--run_cleaning",
             action="store_true",
             help="run obsidian attachments cleaner utility",
         )
         self.parser.add_argument(
-            "-silent",
-            "--silent_cleaning",
-            action="store_false",
-            help="hide a terminal output",
+            "-s",
+            "-show",
+            "--show_output",
+            action="store_true",
+            help="show a terminal output",
         )
         return self.parser.parse_args()
 
@@ -33,5 +35,5 @@ class AttachmentsCleanerCLI:
         if self.arguments.run_cleaning:
             self.cleaner_view.run_attachments_cleaner()
 
-        if not self.arguments.silent_cleaning or self.arguments.run_cleaning:
+        if self.arguments.show_output:
             self.cleaner_view.print_terminal_output()
