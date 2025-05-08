89ebd5b5739793175e9418e7a17d71195eaa4ca5
Aleksander Zizevskikh <zizevskikh.dev@gmail.com>
2025-05-08 04:21:30 +0300

    fixed doc in manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
-"""Main entry point for the Obsidian Attachments Cleaner.
-
-Initializes and runs the command line interface.
-"""
+"""Main entry point for Obsidian Attachments Cleaner."""
 
 from cli import AttachmentsCleanerCLI
 
 
 if __name__ == "__main__":
-    """Entry point for the command line interface."""
+    """Entry point for the CLI application."""
     cli_controller = AttachmentsCleanerCLI()
     cli_controller.run_attachments_cleaner_cli()
